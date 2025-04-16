c52bff19b9e7085559497259d54b51b43116f1da
SeungHyunLee054 <yellow13lsh@naver.com>
2025-04-16 10:30:14 +0900

    레벨 3-1 : 예상대로 성공하는지에 대한 케이스

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -1,28 +1,30 @@
 package org.example.expert.config;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @ExtendWith(SpringExtension.class)
 class PasswordEncoderTest {
 
-    @InjectMocks
-    private PasswordEncoder passwordEncoder;
+	@InjectMocks
+	private PasswordEncoder passwordEncoder;
 
-    @Test
-    void matches_메서드가_정상적으로_동작한다() {
-        // given
-        String rawPassword = "testPassword";
-        String encodedPassword = passwordEncoder.encode(rawPassword);
+	@Test
+	@DisplayName("matches 테스트 성공")
+	void success_matches() {
+		// given
+		String rawPassword = "testPassword";
+		String encodedPassword = passwordEncoder.encode(rawPassword);
 
-        // when
-        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+		// when
+		boolean matches = passwordEncoder.matches(rawPassword, encodedPassword);
 
-        // then
-        assertTrue(matches);
-    }
+		// then
+		assertTrue(matches);
+	}
 }
